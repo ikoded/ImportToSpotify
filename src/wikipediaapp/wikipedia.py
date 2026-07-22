6d2ba7dc1727430aa15f866ce1711a0a781d3001
Koder <45540379+ikoded@users.noreply.github.com>
2026-07-22 16:01:50 -0500

    Wikipedia integration (#1)
    * Update to be more modular for future sources
    
    * Added more wikipedia sources(by year)

--- src/wikipediaapp/wikipedia.py
+++ src/wikipediaapp/wikipedia.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 class Wikipedia:
     # Grab table result based on URL
     @staticmethod
-    def grabTableResult(url: str):
+    def grab_table_result(url: str):
         params = {}
         headers= {"User-Agent": "ImportToSpotifyBot/0.0 (https://github.com/ikoded/ImportToSpotify)"}
         request = requests.get(url,headers=headers)
@@ -15,7 +15,7 @@ class Wikipedia:
         return table
     
     @staticmethod
-    def parsebillboardhot100(table):
+    def parse_billboard_hot_100_2020(table):
         results = {}
         for row in table.find_all('tr'):
             cells = [cell.text.strip() for cell in row.find_all(['td', 'th'])]
@@ -23,6 +23,17 @@ class Wikipedia:
                 continue
             songname = cells[3]
             cleanname = songname.strip('"\' ♪[]0123456789')
+            results[cleanname] = cells[2]
+
+        return results
+
+    @staticmethod
+    def parse_billboard_hot_100_by_year(table):
+        results = {}
+        for row in table.find_all('tr'):
+            cells = [cell.text.strip() for cell in row.find_all(['td', 'th'])]
+            songname = cells[1]
+            cleanname = songname.strip('"\'')
             results[cleanname] = cells[2]
 
         return results
